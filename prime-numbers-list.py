efcc8445531ae29e84768b413adf9ec10359b86f
Hugo Bachér <54435874+hugobacher@users.noreply.github.com>
2019-09-09 13:36:39 +0200

    Create prime-numbers-list.py

---
+++ prime-numbers-list.py
@@ -0,0 +1,10 @@
+topNumber = int(input("Write the number to check to: "))
+for num in range(2, topNumber + 1):
+   #If the number is less than or equal to 1 then it is not prime number.
+   if num > 1:
+       for i in range(2, num):
+         #If the number divided by i is equal to 0 then it's not a prime number.
+           if (num %i == 0):
+               break
+       else:
+           print (num)
