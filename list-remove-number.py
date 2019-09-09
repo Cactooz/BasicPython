fe79c0194425e97ffb3f1032231dccc52c83203d
Hugo Bachér <54435874+hugobacher@users.noreply.github.com>
2019-09-09 13:33:07 +0200

    Create list-remove-number.py

---
+++ list-remove-number.py
@@ -0,0 +1,5 @@
+numbers = [-4.3, -6.7, -0.8, 1.2, 2.1, 2.6, 1.1]
+numbers.sort()
+numbers.remove(numbers[2]) #removes the 3rd number from the list
+numbers.sort()
+print ("3rd number removed, new list " + str(numbers))
